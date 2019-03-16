e97bc0b64c603416bd95b63206f2fd510d48dbb6
Matt <matt@supermatty.com>
2019-03-17 08:53:59 +1100

    Initial import: the logistic recurrence relation, iterated to draw a bifurcation diagram.

---
+++ scripts.js
@@ -0,0 +1,33 @@
+(function($) {
+	function plot(imageData, x, r) {
+		var i;
+		var p = 0.5;
+		// Iterate for a while, discarding points, to permit convergence
+		for (i = 0; i < 1000; i++) {
+			p = r * p * (1 - p);
+		}
+		// Now plot some points
+		for (i = 0; i < imageData.height; i++) {
+			var y = p * (imageData.height);
+			setPixel(imageData, x, y, 0, 0, 0, 255);
+			p = r * p * (1 - p);
+		}
+	}
+	function drawBifurcationDiagram(bifurc, zoom) {
+		var ctx = bifurc[0].getContext('2d');
+		var imageData = ctx.getImageData(0, 0, bifurc.width(), bifurc.height());
+		var rmin = 2.4, rmax = 4;
+		var x;
+		for (x = 1; x < bifurc.width(); x++) {
+			var r = rmin + (rmax - rmin) * (x / bifurc.width());
+			plot(imageData, x, r);
+		}
+		ctx.putImageData(imageData, 0, 0);
+	}
+	$(function() {
+		var bifurc = $('#bifurc');
+		bifurc.attr({width: bifurc.width(), height: bifurc.height()});
+		var zoom = 1;
+		drawBifurcationDiagram(bifurc, zoom);
+	});
+})(jQuery);
